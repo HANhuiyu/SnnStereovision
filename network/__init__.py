15ff1cd5a09b232237bb5a31179c8cfcd51c94da
HANhuiyu <36294954+HANhuiyu@users.noreply.github.com>
2021-05-31 10:22:22 +0200

    Update __init__.py

--- network/__init__.py
+++ network/__init__.py
@@ -3,4 +3,4 @@ from network.cooperative_net import *
 from network.retina import *
 from network.simulation import *
 from network.ext_input import *
-from network.ext_input_hdf5 import *
+#from network.ext_input_hdf5 import *
