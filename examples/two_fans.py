dfde490e3ed0c5d9a5c9639eb25c6a9cfd515363
HANHuiyu <huiyu.han@etu.unice.fr>
2021-06-07 15:39:04 +0200

    size

--- examples/two_fans.py
+++ examples/two_fans.py
@@ -22,11 +22,11 @@ def run_experiment_fans(with_visualization=True):
     """
     experiment_name = "Back_Front"
     experiment_duration = 10000.0  # in ms
-    dx = 30  # in pixels
-    dy = 30  # in pixels
+    dx = 25  # in pixels
+    dy = 25  # in pixels
     max_d = 24  # in pixels
-    crop_xmin = 50  # in pixels
-    crop_ymin = 50  # in pixels
+    crop_xmin = 35  # in pixels
+    crop_ymin = 35  # in pixels
 
     # Setup the simulation
     Simulation = SNNSimulation(simulation_time=experiment_duration)
