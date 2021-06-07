9c2f38dbae10d9f4b19c9bc6aa8c97d2692867ce
HANhuiyu <36294954+HANhuiyu@users.noreply.github.com>
2021-06-07 16:54:49 +0200

    Update two_fans.py

--- examples/two_fans.py
+++ examples/two_fans.py
@@ -22,10 +22,10 @@ def run_experiment_fans(with_visualization=True):
     """
     experiment_name = "Back_Front"
     experiment_duration = 10000.0  # in ms
-    dx = 15  # in pixels
+    dx = 20  # in pixels
     dy = 30  # in pixels
-    max_d = 14  # in pixels
-    crop_xmin = 40  # in pixels
+    max_d = 20  # in pixels
+    crop_xmin = 45  # in pixels
     crop_ymin = 35  # in pixels
 
     # Setup the simulation
