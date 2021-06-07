b9cbbdda36c546c2c370d5ba34814fae1988ee6c
HANhuiyu <36294954+HANhuiyu@users.noreply.github.com>
2021-06-07 16:57:42 +0200

    Update two_fans.py

--- examples/two_fans.py
+++ examples/two_fans.py
@@ -24,7 +24,7 @@ def run_experiment_fans(with_visualization=True):
     experiment_duration = 10000.0  # in ms
     dx = 20  # in pixels
     dy = 30  # in pixels
-    max_d = 20  # in pixels
+    max_d = 19  # in pixels
     crop_xmin = 45  # in pixels
     crop_ymin = 35  # in pixels
 
