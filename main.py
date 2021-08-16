866cebcfd330b93380493c320220a6d945bc1e5e
HANHuiyu <huiyu.han@etu.unice.fr>
2021-08-16 13:26:01 +0200

    desc simulation

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from examples import *
 from network import *
 if __name__ == "__main__":
-    run_experiment_pendulum()
+    #run_experiment_pendulum()
     #run_experiment_fans()
     #run_experiment_nst()
-    #run_desc()
+    run_desc()
